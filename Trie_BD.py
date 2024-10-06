76e523eaaca4824eb3afdb779c54f30c65f4bfeb
Melanie Gomis <melanie.gomis@etu.univ-montp3.fr>
2024-10-06 15:04:52 +0200

    Suppression des colonnes

--- Trie_BD.py
+++ Trie_BD.py
@@ -1,3 +1,4 @@
 import pandas as pd
-data = pd.read_csv("accidents.csv", sep = ";", index_col=0)
-data.head()
+acc = pd.read_csv("accidents.csv", sep = ";", index_col=0)
+acc.drop(columns=["date","original_city_code","original_name","geo_score","distance","v1","v2","pr","pr1","vosp","lartpc","larrout","env1","PR_display"], inplace = True)
+print(acc.head())
